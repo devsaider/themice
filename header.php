8bcd0484a03b0918571247c5505f33f29373fcaf
root <root@mirror.devsaider.ru>
2016-09-25 06:39:36 +0300

    titles

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html>
   <head>
-    <title>Transformice</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title><?php is_front_page() ? bloginfo('description') : wp_title('');?> &raquo; <?php bloginfo('name'); ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset');?>">
     <?php wp_head();?>
   </head>
   <body>
