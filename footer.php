6881e5da21189b75ea2ef77c51551f3604a10f8e
devsaider <devsaider@mirror.devsaider.ru>
2016-09-25 07:29:54 +0300

    added hookevent

--- footer.php
+++ footer.php
@@ -1,4 +1,7 @@
     <div style="clear: both;"></div>
   </div> <!-- end of #page -->
   <?php wp_footer(); ?>
+  <script>
+    hookEvent(document.getElementById("transformice"), "mousewheel", cancelEvent);
+  </script>
 </body>
